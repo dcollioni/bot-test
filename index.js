16d560b1f89daaf4e68313b5109954d32ea7fcd4
Douglas Collioni <dcollioni@gmail.com>
2016-04-19 15:45:40 -0300

    Require request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var messenger = require('./messenger.js');
+var request = require("request");
 var app = express();
 
 app.use(bodyParser.json());
