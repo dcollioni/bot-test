784a12921e49fc4b05cf4bff6e4eb4b788a64110
Douglas Collioni <dcollioni@gmail.com>
2016-04-19 11:06:09 -0300

    GET webhook

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get('/', function(request, response) {
 	response.send('Ok');
 });
 
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'superplayer_lets_rock') {
+		res.send(req.query['hub.challenge']);
+	}
+	res.send('Error, wrong validation token');
+});
+
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
