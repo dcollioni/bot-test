8d9144cb5ced7b36ae6db8d8d5d2d370a0cad206
Douglas Collioni <dcollioni@gmail.com>
2016-04-22 15:30:34 -0300

    Fix bug token missing

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,10 @@
 var request = require("request");
 var Promise = require("bluebird");
 
+var tokenTrid = "CAAHp6HIb7KwBALm6gIKxdoxHiZBEMpUQWZCqn1iWi7WvaFtZCtzZCSpNl2I4xIfSjk0BcL9iz0dckX7XzPnz20fZAgAhRxRmpfgQG1rwvU3c4G0KtJ3aSaIwPDLwYbVcJT8GdtKqltmFemsrwY4hjXIYIlsaXK2GJPLwHZCgVWEfESUuLExhzGobrvCH9DVmuzCRIlGHZB4mgZDZD";
+var tokenSplZak = "CAADH99bPVBUBABUOgWkZBuJivN4atcLc7yrGG5a8vHmvlyhRhDEch6PJH1xCzDIh08bSBIZCfqh1AeUvN2NbVbzkpZBCTLNAFY3xfvJEbe8LETcLs2H2bmgT3ECxbOm9ZCxe6IZADVEmmfSNNWYE4eGFlyfMfGzqpQ8b1ZB8U8HIcKe99LmPQbIfg3rdvv1r2RGqv9KjCH6gZDZD";
+var token = tokenTrid;
+
 var baseApiUrl = "http://splchat-alpha.herokuapp.com";
 var fbGraphUrl = "https://graph.facebook.com/v2.6/";
 
