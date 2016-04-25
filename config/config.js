4c255cfc5cb9d741cb2cb714f99369c8169402a6
Douglas Collioni <dcollioni@gmail.com>
2016-04-25 11:58:10 -0300

    Config port

--- config/config.js
+++ config/config.js
@@ -3,9 +3,8 @@ module.exports = function() {
             verifyToken: "superplayer_lets_rock",
             graphUrl: "https://graph.facebook.com/v2.6"
         },
-        api = {
-
-        },
+        api = {},
+        app = {},
         s3 = {
             cardImage: "https://s3.amazonaws.com/sp.publicdocuments/images/bot-default.jpg"
         };
@@ -14,16 +13,19 @@ module.exports = function() {
     {
         messenger.token = "CAADH99bPVBUBABUOgWkZBuJivN4atcLc7yrGG5a8vHmvlyhRhDEch6PJH1xCzDIh08bSBIZCfqh1AeUvN2NbVbzkpZBCTLNAFY3xfvJEbe8LETcLs2H2bmgT3ECxbOm9ZCxe6IZADVEmmfSNNWYE4eGFlyfMfGzqpQ8b1ZB8U8HIcKe99LmPQbIfg3rdvv1r2RGqv9KjCH6gZDZD";
         api.baseUrl = "http://ec2-54-226-237-234.compute-1.amazonaws.com";
+        app.port = 8002;
     }
     else
     {
         messenger.token = "CAAHp6HIb7KwBAJxahAYNoXuef5LvY4uMcBZCAtKArS9nTaRe8e5N9LmMDse8MBZC7JJEJoPNiZB24IoZBYFh7U63TeA17T2rHzLF41Baxv4eu7A0gs2zHnTuKL6ResLDzwx4CINvehEsPKl1YO7J0TqYoNiYclS2QkI7ufT2fZATwhxYhOrJYW98b41z97A6s0CsarsYNfgZDZD";
         api.baseUrl = "http://splchat-alpha.herokuapp.com";
+        app.port = process.env.PORT || 8002;
     }
 
     return {
         messenger: messenger,
         api: api,
+        app: app,
         s3: s3
     };
 };
