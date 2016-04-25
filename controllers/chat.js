ff09de6f597226ce849a2d01491c0fba23fb2fc2
Douglas Collioni <dcollioni@gmail.com>
2016-04-25 15:37:01 -0300

    console say response body

--- controllers/chat.js
+++ controllers/chat.js
@@ -27,7 +27,8 @@ class ChatController {
         console.log('Error saying: ', response.body.error);
         return deferred.reject();
       }
-      console.log(response.body);
+      console.log('response.body', response.body);
+      console.log('body', body);
       deferred.resolve(response.body);
     });
 
